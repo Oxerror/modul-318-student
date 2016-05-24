322562030d8731ae4425c992755d80a2aae35d92
dok402 <dok402@DOK402>
2016-05-24 15:07:09 +0200

    form

---
+++ SearchWindow/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace SearchWindow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdSearch = new System.Windows.Forms.Button();
+            this.Searchbox = new System.Windows.Forms.TextBox();
+            this.listResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cmdSearch
+            // 
+            this.cmdSearch.Location = new System.Drawing.Point(170, 39);
+            this.cmdSearch.Name = "cmdSearch";
+            this.cmdSearch.Size = new System.Drawing.Size(75, 23);
+            this.cmdSearch.TabIndex = 0;
+            this.cmdSearch.Text = "Search";
+            this.cmdSearch.UseVisualStyleBackColor = true;
+            this.cmdSearch.Click += new System.EventHandler(this.cmdSearch_Click);
+            // 
+            // Searchbox
+            // 
+            this.Searchbox.Location = new System.Drawing.Point(52, 41);
+            this.Searchbox.Name = "Searchbox";
+            this.Searchbox.Size = new System.Drawing.Size(100, 20);
+            this.Searchbox.TabIndex = 1;
+            // 
+            // listResult
+            // 
+            this.listResult.FormattingEnabled = true;
+            this.listResult.Location = new System.Drawing.Point(52, 81);
+            this.listResult.Name = "listResult";
+            this.listResult.Size = new System.Drawing.Size(184, 160);
+            this.listResult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.listResult);
+            this.Controls.Add(this.Searchbox);
+            this.Controls.Add(this.cmdSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdSearch;
+        private System.Windows.Forms.TextBox Searchbox;
+        private System.Windows.Forms.ListBox listResult;
+    }
+}
+
